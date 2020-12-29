8a61ca6216f35e0cae7522f65bce40ac51959513
rileydakota <dakotariley2@gmail.com>
2020-12-29 16:33:26 -0500

    feat: Create local construct for ThreatDataFetcher

--- lib/aws-s3-threat-data-aggregator-stack.ts
+++ lib/aws-s3-threat-data-aggregator-stack.ts
@@ -1,35 +1,35 @@
-import * as cdk from '@aws-cdk/core';
-import * as s3 from '@aws-cdk/aws-s3';
-import * as lambda from '@aws-cdk/aws-lambda-nodejs';
-import { PolicyStatement } from '@aws-cdk/aws-iam';
-import { Rule, Schedule } from '@aws-cdk/aws-events';
-import { LambdaFunction } from '@aws-cdk/aws-events-targets';
+import * as cdk from "@aws-cdk/core";
+import * as s3 from "@aws-cdk/aws-s3";
+import { Interval, ThreatDataFetcher } from './threat-data-fetcher'
+
+
 
 export class AwsS3ThreatDataAggregatorStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const ThreatDataBucket = new s3.Bucket(this, 'ThreatDataBucket', {
+    const ThreatDataBucket = new s3.Bucket(this, "ThreatDataBucket", {
       encryption: s3.BucketEncryption.S3_MANAGED,
     });
-    const ThreatDataFetcher = new lambda.NodejsFunction(this, 'ThreatDataFetcherFunction', {
-      handler: 'handler',
-      entry: 'lib/functions/threat-data-fetcher-function.ts',
-      environment: {
-        BUCKET_NAME: ThreatDataBucket.bucketName,
-        DATA_SOURCE_URL: 'https://check.torproject.org/torbulkexitlist',
-        DATA_SOURCE_NAME: 'TOR_EXIT_NODES'
-      }
-    });
-    ThreatDataFetcher.addToRolePolicy( new PolicyStatement({
-      actions: ['s3:PutObject'],
-      resources: [`${ThreatDataBucket.bucketArn}/TOR_EXIT_NODES/*`]
-    }))
-    const LambdaTarget = new LambdaFunction(ThreatDataFetcher)
-    const LmabdaTrigger = new Rule(this, 'ThreatDataFetcherTrigger', {
-      schedule: Schedule.cron({minute: '0', hour: '0-23'}),
-      targets: [LambdaTarget]
-    })
     
+    
+    //TOR Data
+    
+    const TorExitNodeFetcher = new ThreatDataFetcher(this, 'TorExitNodeFetcher', {
+      Bucket: ThreatDataBucket,
+      DataSourceUrl: 'https://check.torproject.org/torbulkexitlist',
+      DataSourceName: 'TOR_EXIT_NODES',
+      Interval: Interval.Hourly
+    })
+
+    //IPSUM Data
+
+    const IpsumDataFetcher = new  ThreatDataFetcher(this, 'IpsumDataFetcher', {
+      Bucket: ThreatDataBucket,
+      DataSourceUrl: 'https://raw.githubusercontent.com/stamparm/ipsum/master/ipsum.txt',
+      DataSourceName: 'IPSUM',
+      Interval: Interval.Daily
+    })
+
   }
 }
--- lib/functions/threat-data-fetcher-function.ts
+++ lib/functions/threat-data-fetcher-function.ts
@@ -1,44 +1,37 @@
-import * as axios from 'axios'
-import S3 from 'aws-sdk/clients/s3'
-
-
+import * as axios from "axios";
+import S3 from "aws-sdk/clients/s3";
 
 //https://check.torproject.org/torbulkexitlist
 //https://intercept.sh/threatlists/ -token required
 //https://raw.githubusercontent.com/stamparm/ipsum/master/ipsum.txt -probably needs to be its own alert - aggregates aton of threat intel data
 
-
-async function GetTorExitNodes(url: string ) {
-   console.info(`making GET request to specified URL ${url}`)
-   const result = await axios.default.get(url)
-   return result.data
+async function GetTorExitNodes(url: string) {
+  console.info(`making GET request to specified URL ${url}`);
+  const result = await axios.default.get(url);
+  return result.data;
 }
-
-
 
 export const handler = async (event: any) => {
-   const bucket: string = process.env.BUCKET_NAME!
-   const dataSourceUrl: string = process.env.DATA_SOURCE_URL!
-   const dataSourceName: string = process.env.DATA_SOURCE_NAME!
+  const bucket: string = process.env.BUCKET_NAME!;
+  const dataSourceUrl: string = process.env.DATA_SOURCE_URL!;
+  const dataSourceName: string = process.env.DATA_SOURCE_NAME!;
 
-   console.info(`saving data to bucket: ${bucket}`)
-   console.info(`target URL: ${dataSourceUrl}`)
-   console.info(`specified Data Source Name: ${dataSourceName}`)
+  console.info(`saving data to bucket: ${bucket}`);
+  console.info(`target URL: ${dataSourceUrl}`);
+  console.info(`specified Data Source Name: ${dataSourceName}`);
 
-   
+  const data = await GetTorExitNodes(dataSourceUrl);
 
-   const data = await GetTorExitNodes(dataSourceUrl)
-   
-   var uploadParams: S3.PutObjectRequest = {
-      Body: data,
-      Bucket: bucket,
-      Key: `${dataSourceName}/data.txt`
-   }
-   const s3 = new S3();
+  var uploadParams: S3.PutObjectRequest = {
+    Body: data,
+    Bucket: bucket,
+    Key: `${dataSourceName}/data.txt`,
+  };
+  const s3 = new S3();
 
-   console.info('attempting upload to bucket')
+  console.info("attempting upload to bucket");
 
-   const result = await s3.upload(uploadParams).promise()
-   
-   console.info(`success! Data uploaded to ${result.Location}`)   
-}
+  const result = await s3.upload(uploadParams).promise();
+
+  console.info(`success! Data uploaded to ${result.Location}`);
+};
